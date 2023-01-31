7839992b08b5b2e4b769966a6ccf30be10bb75fc
rayoub <ronaldayoub@gmail.com>
2023-01-31 07:52:44 -0600

    commit

--- src/main/java/edu/cmol/web/api/IonResource.java
+++ src/main/java/edu/cmol/web/api/IonResource.java
@@ -8,11 +8,13 @@ import javax.json.stream.JsonGenerator;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
+import edu.kumc.ion.db.QueryCriteria;
 import edu.kumc.ion.db.Reporter;
 import edu.kumc.ion.db.Variant;
 
@@ -23,13 +25,18 @@ public class IonResource extends BaseResource {
 
     @GET
     @Produces("application/json")
-    public Response get() throws Exception {
+    public Response get(
+        @QueryParam("sample") String sample
+    ) throws Exception {
     
         // get response json
         String json = "{}";
         try {
-    
-            List<Variant> rows = Reporter.getVariants();
+   
+            QueryCriteria criteria = new QueryCriteria();
+            criteria.setSample(sample);            
+
+            List<Variant> rows = Reporter.getVariants(criteria);
 
             if (rows.size() > MAX_QUERY_ROWS) {
 
